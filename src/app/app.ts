e1b2da75fa9daf6ac0ac321e94165e3152d1fe91
Jackboles <bolesjack399@gmail.com>
2025-09-26 12:05:40 -0400

    Create HTML template of chosen data type. Display data to the screen

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {Record} from "./Shared/models/record";
+import {JsonPipe, ngFor, ngIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ngFor, ngIf, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('JackBolesLearningAngular');
+  title = 'Record Store';
 
-  firstName:string = 'Jack';
-  lastName:string = 'Boles';
+  records: Record[] = [
+    {title: 'Abbey Road', artist: 'The Beatles', genre: 'Rock', price: 29.99, releaseYear: 1969},
+    {title: 'Nevermind', artist: 'Nirvana', genre: 'Grunge', price: 34.99, releaseYear: 1991},
+    {title: 'Back In Black', artist: 'AC/DC', genre: 'Rock', price: 27.99, releaseYear: 1980},
+    {title: 'Dookie', artist: 'Green Day', genre: 'Punk', price: 29.99},
+    {title: 'The Dark Side of the Moon', artist: 'Pink Floyd', genre: 'Progressive Rock', price: 30.00, releaseYear: 1973},
+    {title: 'Rumours', artist: 'Fleetwood Mac', genre: 'Rock', price: 25.99}];
 }
