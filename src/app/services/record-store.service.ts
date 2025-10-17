99f0c2d199b047e2fab00e86301c47c08e356e69
Jackboles <bolesjack399@gmail.com>
2025-10-17 09:58:04 -0400

    implement full CRUD methods in RecordStoreService

--- src/app/services/record-store.service.ts
+++ src/app/services/record-store.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {Record} from "../Shared/models/record";
+import {recordList} from "../data/mock-records";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecordStoreService {
-  
+  private records: Record[] = recordList;
+  constructor() {}
+    getRecords(): Observable<Record[]> {
+      return of(recordList);
+  }
+
+  getRecordById(id: number): Observable<Record | undefined> {
+    const record = this.records.find(r => r.id === id);
+    return of(record);
+  }
+
+  addRecord(newRecord: Record): Observable<Record[]> {
+    this.records.push(newRecord);
+    return of(this.records);
+  }
+
+  updateRecord(updatedRecord: Record): Observable<Record[]> {
+    const index = this.records.findIndex(r => r.id === updatedRecord.id);
+    if (index !== -1) {
+      this.records[index] = updatedRecord;
+    }
+    return of(this.records);
+  }
+
+  deleteContent(id: number): Observable<Record[]> {
+    this.records = this.records.filter(r => r.id !== id);
+    return of(this.records);
+  }
 }
