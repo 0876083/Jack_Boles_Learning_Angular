74c296c68d1d17728dc51b89b2a1f3eb69e0b829
Jackboles <bolesjack399@gmail.com>
2025-10-17 08:56:15 -0400

    inject RecordStoreService into the record-list component

--- src/app/record-list/record-list.component.ts
+++ src/app/record-list/record-list.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Record} from '../Shared/models/record';
+import {NgForOf} from "@angular/common";
+import {RecordListItemComponent} from '../record-list-item/record-list-item.component';
+import {RecordStoreService} from "../services/record-store.service";
 
 @Component({
   selector: 'app-record-list',
-  imports: [],
+  standalone: true,
+  imports: [NgForOf, RecordListItemComponent],
   templateUrl: './record-list.component.html',
   styleUrl: './record-list.component.scss'
 })
-export class RecordListComponent {
+export class RecordListComponent implements OnInit {
+recordList: Record[] = [];
+constructor(private recordService: RecordStoreService) {}
 
+  ngOnInit(): void {
+  this.recordService.getRecords().subscribe({
+    next: (data: Record[]) => this.recordList = data,
+    error:err => console.error("Error fetching Records", err),
+    complete:() => console.log("Record data fetch complete!"),
+  })
+  }
 }
