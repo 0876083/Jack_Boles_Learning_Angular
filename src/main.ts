9266e9f7f6deffa936a534fb3ee683bd589e4d00
Jackboles <bolesjack399@gmail.com>
2025-10-17 12:14:27 -0400

    set up routing and added rout definitions

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import {provideRouter, Routes} from '@angular/router';
+import {AppComponent} from './app/app';
+import {RecordListItemComponent} from './app/record-list-item/record-list-item.component';
+import {RecordListComponent} from './app/record-list/record-list.component';
+import {ModifyRecordComponent} from './app/modify-record/modify-record.component';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+
+const routes: Routes = [
+  {path: '', redirectTo: '/records', pathMatch: 'full'},
+  {path: 'records', component: RecordListComponent},
+
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+});
+
